a0e01581a807c9453c3cb2486fcc2d5da7fc9d61
Connor-OHara <52166249+Connor-OHara@users.noreply.github.com>
2023-10-10 01:12:52 -0400

    Add project files.

---
+++ BarnesHuttGravity/main.cpp
@@ -0,0 +1,25 @@
+/*
+	Connor O'Hara
+	Main Program for Barnes-Hut simulation.
+
+	This shows an approximation of a N-body problem using the Barnes-Hut octree
+	implementation for O(nlog(n)) time complexity. It should not be used for 
+	precise calulation for specific bodies, but will suffice for observing system-level 
+	effects.
+
+*/
+
+#include <stdio.h>
+#include <omp.h>
+
+/*
+* This is the main function that acts as a dispatcher for the subprocesses that will manage 
+* The running of the simulation. 
+*/
+int main(int argc, char** argv) 
+{
+
+
+
+
+}
---
+++ BarnesHuttGravity/particle.h
@@ -0,0 +1,57 @@
+#pragma once
+/*
+Author: Connor O'Hara
+Class : ECE4122
+Last Date Modified : 10/10/2023
+Description :
+This is the file that defines a Particle. 
+*/
+
+
+
+#include <vector>
+
+using namespace std;
+
+#ifndef N_PART
+#define N_PART
+
+/*
+* Description:
+* Header class describing important member functions, constructor, and descriptors for the class.
+* The Point Charge Class describes a singular point charge in 3D space, giving it attributes about its location and charge quantity.
+*/
+class Particle
+{
+public:
+
+	//Default Constructors with overloads.
+	Particle(double x_, double y_, double z_, double mass);
+	Particle(double x_, double y_, double z_, double mass, double acc);
+	Particle() : x(0.0), y(0.0), z(0.0), mass(0.0) {};
+
+	// Sets the default location 
+	void setLocation(double x_, double y_, double z_);
+	//returns a pointer to an array where arr[0] == x, arr[1] == y, arr[2] == z. Discrete indice for O(1)
+	std::vector<double> getLocation() const;
+
+	//getter and setter for mass
+	void setMass(double mass);
+	double getMass() const;
+
+	double getX() const;
+	double getY() const;
+	double getZ() const;
+
+
+	void setAcc(double acc);
+	double getAcc();
+
+protected:
+	/// <summary>
+	///	Protected attributes.
+	/// </summary>
+	double x, y, z, mass, acceleration;
+};
+
+#endif 
